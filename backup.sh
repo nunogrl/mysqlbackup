ff57960564898b93e433432441ce2d9e2a17d1f1
Newname <new@email>
2016-07-02 20:48:55 +0100

    changed to reads information from external ini file

--- backup.sh
+++ backup.sh
@@ -1,21 +1,40 @@
 #!/bin/bash
 
-FILE=bc_`date +"%F_%H-%M"`.sql.gz
-DBSERVER=127.0.0.1
-DATABASE=databasename
-USER=databaseuser
-PASS=databasepass
+CONFIGFILE=/opt/backup/backup.ini
 BACKUP=/opt/backup/
-REMOTEFOLDER=remotefolder
+
+function ini_get
+{
+    eval `sed -e 's/[[:space:]]*\=[[:space:]]*/=/g' \
+        -e 's/;.*$//' \
+        -e 's/[[:space:]]*$//' \
+        -e 's/^[[:space:]]*//' \
+        -e "s/^\(.*\)=\([^\"']*\)$/\1=\"\2\"/" \
+    < $CONFIGFILE \
+    | sed -n -e "/^\[$1\]/,/^\s*\[/{/^[^;].*\=.*/p;}"`
+
+    echo ${!2}
+}
+
+# if there's no file, quits
+[ ! -f $CONFIGFILE ] && exit 0;
+
+# Get information from file
+file=$(ini_get MYSQLBACKUP file)
+dbserver=$(ini_get MYSQLBACKUP dbserver)
+user=$(ini_get MYSQLBACKUP user)
+pass=$(ini_get MYSQLBACKUP pass)
+database=$(ini_get MYSQLBACKUP database)
+remotefolder=$(ini_get MYSQLBACKUP remotefolder)
 
 
-mysqldump --opt --user=${USER} --password=${PASS} ${DATABASE} | /usr/bin/pigz  >  ${BACKUP}${FILE}
+mysqldump --opt --user=${user} --password=${pass} ${database} | /usr/bin/pigz  >  ${BACKUP}${file}
 
 # Upload file
-(cd ${BACKUP} && . ./Dropbox-Uploader/dropbox_uploader.sh -q upload ${FILE} ${REMOTEFOLDER} )
+(cd ${BACKUP} && . ./Dropbox-Uploader/dropbox_uploader.sh -q upload ${file} ${remotefolder} )
 
 
 # If sucessfuly uploaded; remove local copy 
-${BACKUP}/Dropbox-Uploader/dropbox_uploader.sh list  {$REMOTEFOLDER}   | /bin/grep -q  ${FILE} && rm  ${BACKUP}${FILE}
+${BACKUP}/Dropbox-Uploader/dropbox_uploader.sh list  {$remotefolder}  | /bin/grep -q  ${file} && rm  ${BACKUP}${file}
 
 
