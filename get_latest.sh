e285e408c45db5317c0360fabdc7d063bf69ca5a
Nuno Leitao <n.piglet@gmail.com>
2018-04-16 16:01:12 +0100

    get_latest.sh - fixed path

--- get_latest.sh
+++ get_latest.sh
@@ -1,7 +1,6 @@
 #!bin/sh
 
-latest=$(/opt/bctools/dropbox_uploader.sh list barbearclassico | tail -n1 | cut -d' ' -f4)
-
+latest=$(/opt/bctools/Dropbox-Uploader/dropbox_uploader.sh list barbearclassico | tail -n1 | cut -d' ' -f4)
 echo $latest 
 mkdir -p /opt/income
-/opt/bctools/dropbox_uploader.sh download barbearclassico/$latest /opt/income/.
+/opt/bctools/Dropbox-Uploader/dropbox_uploader.sh download barbearclassico/$latest /opt/income/.
